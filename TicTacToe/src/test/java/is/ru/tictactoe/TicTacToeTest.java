3bb503f6d32552179804b3038a9e2fd60a02da8d
arona12 <arona12@ru.is>
2013-11-20 15:36:32 +0000

    Test update

--- TicTacToe/src/test/java/is/ru/tictactoe/TicTacToeTest.java
+++ TicTacToe/src/test/java/is/ru/tictactoe/TicTacToeTest.java
@@ -8,11 +8,21 @@ public class TicTacToeTest{
 	@Test
 	public void genTableTest(){
 		TicTacToe game = new TicTacToe();
-		game.genTable();
 		for(int row = 0; row < 3; row++){
 			for(int column = 0; column < 3; column++){
 				assertEquals("Default value of squares should be 100", 100, game.table[row][column]);
 			}
 		}
+	}
+
+	@Test
+	public void setMarkerTest(){
+		TicTacToe game = new TicTacToe();
+		
+		game.player = true;
+		assertEquals("If player if true then setMarker should return 1", 1, setMarker());
+
+		game.player = false;
+		assertEquals("If player if fasle then setMarker should return 0", 0, setMarker());
 	}
 }
